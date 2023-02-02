9c0dcb3c74af2636bedf330d2d51dd709dfbc92d
whtnqls124578 <whtnqls124578@naver.com>
2023-02-03 05:00:15 +0900

    fix: pages/main/card svg icon 안보이는 오류 수정완료 - card와 header의 icon svg파일 화면 뿌리기 수정 작업입니다. - img 태그를 지우고 svg 파일들이 담겨있는 iconImage를 그대로 태그명으로 수정했습니다. related: #12

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -9,24 +9,25 @@ const Header = () => {
     return (
         <div className={headerStyle.header_wrap}>
             <div className={headerStyle.header_logo}>
-                <a href='' onClick={() => router.push('/')}><img src={iconImage.logo}/></a>
+                <a href='' onClick={() => router.push('/')}>
+                <iconImage.logo/></a>
             </div>
             <div className={headerStyle.header_right}>
                 <button className={headerStyle.night_mode}>
                     <div className={headerStyle.night_mode_div}>
                         <div className={headerStyle.night_mode_rotate}>
-                            <img src={iconImage.nightMode} className={headerStyle.night_mode_icon} />
+                            <iconImage.nightMode className={headerStyle.night_mode_icon} />
                         </div>
                     </div>
                 </button>
                 <a className={headerStyle.search}>
-                    <img src={iconImage.search} width={18} height={18}/>
+                    <iconImage.search width={18} height={18}/>
                 </a>
                 <button className={headerStyle.write_btn} onClick={()=>router.push('/write')}>새 글 작성</button>
                 <div>
                     <div className={headerStyle.profile}>
-                        <img src={iconImage.logo} className={headerStyle.profile_img} />
-                        <img src={iconImage.profile} className={headerStyle.profile_svg} width={10} height={5} />
+                        <iconImage.logo className={headerStyle.profile_img} />
+                        <iconImage.profile className={headerStyle.profile_svg} width={10} height={5} />
                     </div>
                 </div>
             </div>
